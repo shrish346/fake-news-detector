244143e80ce1560e52d204b1e146df6f8949f58f
Shrish Vishnu Rajesh Kumar <shrishvishnu06@gmail.com>
2025-08-22 13:38:35 -0500

    added the model and updated API to include /predict route

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,26 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from model import predict
+
+app = FastAPI()
+
+# To recieve a JSON, input needs to be wrapped in a pydantic model
+class TextRequest(BaseModel):
+    text: str
+
+
+@app.get("/")
+def read_root():
+    return {"message" : "Welcome to the Fake News Detector API!"}
+
+
+@app.post("/predict")
+def predict_news(request: TextRequest):
+    result = predict(request.text)
+    return{
+        "label": result['label'],
+        "score": result['score']
+    }
+
+
 
