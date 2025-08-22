f96fc369dfb45cbe6ee1763a310eb9bdce784a43
Shrish Vishnu Rajesh Kumar <shrishvishnu06@gmail.com>
2025-08-22 13:40:03 -0500

    added model and created model map

--- backend/model.py
+++ backend/model.py
@@ -1,1 +1,19 @@
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+
+pipe = pipeline("text-classification", model="jy46604790/Fake-News-Bert-Detect")
+classifier = pipe
+
+# Map model's label to readable string
+label_map = {
+    "LABEL_0" : "FAKE", "LABEL_1" : "REAL"
+}
+
+# Function that predicts the label and score for a given text
+def predict(text):
+    result = classifier(text)[0]
+    return{
+        "label": label_map[result['label']],
+        "score": result['score']
+    }
 
